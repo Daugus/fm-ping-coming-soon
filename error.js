73cbf5b1e4c7216c27fc5285550b2147b7ce4f22
daugus <augustodelacamara@gmail.com>
2022-03-07 16:54:03 +0100

    fixed script

--- error.js
+++ error.js
@@ -1,17 +1,21 @@
-const email = document.querySelector('.notify__email');
-const input = document.querySelector('.notify__button');
+const input = document.querySelector('.notify__email');
+const button = document.querySelector('.notify__button');
 const message = document.querySelector('#message');
 
-input.addEventListener('click', showMessage);
+input.addEventListener('keyup', function (event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    button.click();
+  }
+});
+button.addEventListener('click', showMessage);
 
 function showMessage() {
-  if (email.checkValidity()) {
-    console.log(true);
-    email.classList.remove('invalid');
+  if (input.checkValidity()) {
+    input.classList.remove('invalid');
     message.classList.remove('error');
   } else {
-    console.log(false);
-    email.classList.add('invalid');
+    input.classList.add('invalid');
     message.classList.add('error');
   }
 }
